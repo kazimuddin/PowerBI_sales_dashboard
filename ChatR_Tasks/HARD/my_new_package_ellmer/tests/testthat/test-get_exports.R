1cd70879fb3dfb3ed60228789806c3f11f8bde3e
Mohammad Kazimuddin <kazeemalam@gmail.com>
2025-05-27 20:20:10 +0530

    first commit

--- ChatR_Tasks/HARD/my_new_package_ellmer/tests/testthat/test-get_exports.R
+++
@@ -1,7 +0,0 @@
-install.packages("testthat")
-library(testthat)
-test_that("get_exported_functions returns a response", {
-  res <- get_exported_functions("ggplot2")
-  expect_true(is.character(res) || is.list(res))
-})
-
